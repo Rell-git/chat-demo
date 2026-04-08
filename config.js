0de985dd9505761dcf393a6e8f31a0feea6a9a4f
i-Rell <rayvenblazo@gmail.com>
2026-04-08 12:36:29 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
-const SUPABASE_URL = 'https://yptmcwxvxgvqpzvvtnxv.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
-
-const client = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const SUPABASE_URL = 'https://yptmcwxvxgvqpzvvtnxv.supabase.co'; // based sa key mo
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwdG1jd3h2eGd2cXB6dnZ0bnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MTQxMjksImV4cCI6MjA5MTE5MDEyOX0.HPLKZzAO2_dS9UKFtxdWsvLfyf5syDx9kYZyYCapCtg'; 
