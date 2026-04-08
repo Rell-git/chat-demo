de6605d01d9d221e49d33496c97180c109fed2fd
i-Rell <rayvenblazo@gmail.com>
2026-04-08 12:11:27 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-const SUPABASE_KEY = 'YOUR_ANON_PUBLIC_KEY';
+const SUPABASE_URL = 'https://yptmcwxvxgvqpzvvtnxv.supabase.co'; // based sa key mo
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwdG1jd3h2eGd2cXB6dnZ0bnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2MTQxMjksImV4cCI6MjA5MTE5MDEyOX0.HPLKZzAO2_dS9UKFtxdWsvLfyf5syDx9kYZyYCapCtg';
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
